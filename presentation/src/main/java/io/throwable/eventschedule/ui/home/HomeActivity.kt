94245cdf9dc6d9222efe66d763d59d9fc6f2357a
lanre01 <olanre12waju@gmail.com>
2018-08-22 22:26:59 +0100

    Add toolbar to all navigation pages to match the design specs

--- presentation/src/main/java/io/throwable/eventschedule/ui/home/HomeActivity.kt
+++ presentation/src/main/java/io/throwable/eventschedule/ui/home/HomeActivity.kt
@@ -5,10 +5,13 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import io.throwable.eventschedule.R
 import kotlinx.android.synthetic.main.activity_home.*
 import io.throwable.eventschedule.utils.BottomNavigationHelper
 import kotlinx.android.synthetic.main.app_bar_layout.*
+import kotlinx.android.synthetic.main.app_toolbar_extended.*
 
 
 class HomeActivity : AppCompatActivity(), SpeakerDetailFragment.OnFragmentInteractionListener {
@@ -24,16 +27,26 @@ class HomeActivity : AppCompatActivity(), SpeakerDetailFragment.OnFragmentIntera
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_schedule -> {
+                popFragment()
+                large_toolbar.visibility = VISIBLE
+                titleLarge.text = ("Schedule")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_venue -> {
+                popFragment()
+                large_toolbar.visibility = VISIBLE
+                titleLarge.text = ("Venue")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_speaker -> {
                 setFragment(SpeakerDetailFragment())
+                large_toolbar.visibility = GONE
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_news -> {
+                popFragment()
+                large_toolbar.visibility = VISIBLE
+                titleLarge.text = ("News")
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -48,14 +61,22 @@ class HomeActivity : AppCompatActivity(), SpeakerDetailFragment.OnFragmentIntera
     }
 
     private fun setFragment(fragment: Fragment) {
+        popFragment()
+        supportFragmentManager
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_ENTER_MASK)
+                .replace(R.id.fragment_container, fragment, ROOT_FRAGMENT)
+                .commit()
+    }
+
+    private fun popFragment() {
         for (i in 0 until supportFragmentManager.backStackEntryCount) {
             supportFragmentManager.popBackStack()
         }
-        supportFragmentManager
-                .beginTransaction()
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .replace(R.id.fragment_container, fragment, ROOT_FRAGMENT)
-                .commit()
+        val fragment = supportFragmentManager.findFragmentByTag(ROOT_FRAGMENT)
+
+        if (fragment != null)
+            supportFragmentManager.beginTransaction().remove(fragment).commit()
     }
 
 }
