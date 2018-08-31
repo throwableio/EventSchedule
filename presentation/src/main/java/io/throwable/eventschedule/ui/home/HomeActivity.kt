f10e020f11269cdefa58b112085fd92cc2279fd1
lanre01 <olanre12waju@gmail.com>
2018-08-31 10:53:47 +0100

    start implementation of google sign-in check on startup, complete sign in dialog

--- presentation/src/main/java/io/throwable/eventschedule/ui/home/HomeActivity.kt
+++ presentation/src/main/java/io/throwable/eventschedule/ui/home/HomeActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import io.throwable.eventschedule.R
 import io.throwable.eventschedule.ui.schedule.ScheduleFragment
 import io.throwable.eventschedule.ui.speakers.SpeakerDetailFragment
@@ -63,6 +68,34 @@ class HomeActivity : AppCompatActivity(), SpeakerDetailFragment.OnFragmentIntera
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         BottomNavigationHelper.disableShiftMode(navigation)
         navigation.selectedItemId = R.id.navigation_schedule
+        showSignInDialog()
+    }
+
+    private fun showSignInDialog() {
+        val inflater = LayoutInflater.from(this)
+        val root = inflater.inflate(R.layout.google_sign_in_dialog, null)
+        val signIn: Button = root.findViewById(R.id.sign_in)
+        val cancel: TextView = root.findViewById(R.id.cancel)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setView(root)
+                .setCancelable(false)
+        val dialog = builder.create()
+
+        signIn.setOnClickListener{
+            dialog.dismiss()
+            Toast.makeText(this, "Waiting on you guys!", Toast.LENGTH_SHORT).show()
+        }
+
+        cancel.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this, "No personalized Schedule for you be that ooo",
+                    Toast.LENGTH_LONG).show()
+        }
+
+        dialog.show()
+
+
     }
 
     private fun setFragment(fragment: Fragment) {
